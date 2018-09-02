eb903a5dbf2bb15f441a2c9f06236f66a0800d99
xetrov05 <33636753+xetrov05@users.noreply.github.com>
2018-09-02 15:43:28 +0100

    Created the navbar hover style
    Navbar now displays content on hover - **NEED TO FIX FOR MOBILE**

--- site/scripts/tetria.js
+++ site/scripts/tetria.js
@@ -38,13 +38,11 @@ var tetria = {
 
         navbar: {
             init() {
-                $(".navbar ul").on("click", function(e) {
+                $(".navbar ul li").on("mouseenter",function(e) {
                     var arr = e.target.id.split("-");
                     var name = arr[arr.length - 1];
-                    var prevEle = $("#navbar-" + tetria.curPage)[0];
-                    var curEle = $("#navbar-" + name)[0];
-                    if (prevEle != null) prevEle.classList.toggle("active");
-                    if (curEle != null) curEle.classList.toggle("active");
+                    var activeEle = $("#navbar-" + name)[0];
+                    console.log(activeEle);
                     $("#navbar-title")[0].innerHTML = name.substring(0, 1).toUpperCase() + name.substring(1);
                     tetria.changeContent(name);
                 });
