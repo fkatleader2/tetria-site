9874b3ec5284c2c92a37ac0e87cf7425966ae873
xetrov05 <33636753+xetrov05@users.noreply.github.com>
2018-09-05 17:54:09 +0100

    reverted

--- site/scripts/tetria.js
+++ site/scripts/tetria.js
@@ -1,13 +1,3 @@
-function nav(e)
-{
-                var arr = e.target.id.split("-");
-                    var name = arr[arr.length - 1];
-                    var activeEle = $("#navbar-" + name)[0];
-                    $("#navbar-title")[0].innerHTML = name.substring(0, 1).toUpperCase() + name.substring(1);
-                    tetria.changeContent(name);
-
-}
-
 var tetria = {
     modules: {},
     curPage: "welcome",
@@ -26,7 +16,7 @@ var tetria = {
         newContent = newContent.toLowerCase();
         var prevEle = $("#content-area #content-" + this.curPage)[0];
         var curEle = $("#content-area #content-" + newContent)[0];
-        if (prevEle == null || curEle == null) { console.log("Error trying to change content"); if (this.debug) {console.trace()} return;}
+        if (prevEle == null || curEle == null) { console.log("Error trying to change content"); if (this.debug) { console.trace() } return; }
         prevEle.classList.toggle("hide");
         curEle.classList.toggle("hide");
         this.curPage = newContent;
@@ -47,25 +37,19 @@ var tetria = {
         },
 
         navbar: {
-            init() 
-            {
-                if(window.width <= 768)
-                {
-                    $(".navbar ul li").on("mouseenter", function(e)
-                    {
-                        nav(e);
-                    });
-                }
-                else
-                {
-                    $(".navbar ul li").on("click", function(e)
-                    {
-                        nav(e);
-                    })
-                };
+            init() {
+
+                $(".navbar ul li").on("click", function (e) {
+                    var arr = e.target.id.split("-");
+                    var name = arr[arr.length - 1];
+                    var activeEle = $("#navbar-" + name)[0];
+                    $("#navbar-title")[0].innerHTML = name.substring(0, 1).toUpperCase() + name.substring(1);
+                    tetria.changeContent(name);
+                })
+
             },
 
-            
+
         },
 
         profbar: {
@@ -73,7 +57,7 @@ var tetria = {
                 this.play();
             },
             play() {
-                $("#play-tetria").on("click", function(e) {
+                $("#play-tetria").on("click", function (e) {
                     window.location.href = '../game/index.html';
                 });
             }
@@ -82,15 +66,15 @@ var tetria = {
         hamburger: {
             init() {
                 // on change
-                $(".hamburger").on("click", function(e) {
+                $(".hamburger").on("click", function (e) {
                     e.currentTarget.classList.toggle("change");
                 });
                 // navbar hamburger show navbar
-                $("#navbar-hamburger").on("click", function(e) {
+                $("#navbar-hamburger").on("click", function (e) {
                     $(".navbar ul")[0].classList.toggle("mobile-hide");
                 });
 
-                $(".navbar ul").on("click", function(e) {
+                $(".navbar ul").on("click", function (e) {
                     $(".navbar ul")[0].classList.add("mobile-hide");
                     $("#navbar-hamburger")[0].classList.remove("change");
                 });
@@ -103,6 +87,6 @@ var tetria = {
     }
 };
 
-$(document).ready(function(e) {
+$(document).ready(function (e) {
     tetria.init();
 });
